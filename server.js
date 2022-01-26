76f752aeb5f9d6b505483823917f6a2e3052ca5a
Tyler Bantley <tyler.bantley@icloud.com>
2022-01-25 22:12:10 -0600

    initialize app function, logged and working

--- server.js
+++ server.js
@@ -1,11 +1,79 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
+mysql = require('mysql2');
 const inquirer = require('inquirer');
 const ct = require('console.table');
+const PORT = process.env.PORT || 3005;
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3200,
-    user: 'root',
-    password: 'root',
-    database: 'employee'
-});
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      
+      user: 'root',
+    
+      password: 'root',
+      database: 'employees'
+    },
+    //console.log(`worked.`)
+  );
+
+  db.connect(function (err) {
+    if (err) throw err;
+    initialize();
+})
+
+const initialize = async () => {
+    try {
+        let answer = await inquirer.prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role',
+                'Exit'
+            ]
+        });
+        switch (answer.action) {
+            case 'View Employees':
+                viewEmp();
+                break;
+
+            case 'View Departments':
+                viewDep();
+                break;
+
+            case 'View Roles':
+                viewRole();
+                break;
+
+            case 'Add Employees':
+                addEmp();
+                break
+
+            case 'Add Departments':
+                addDep();
+                break
+
+            case 'Add Roles':
+                addRole();
+                break
+
+            case 'Update Employee Role':
+                updateEmp();
+                break
+
+            case 'Exit':
+                db.end();
+                break;
+        };
+    } catch (err) {
+        console.log(err);
+        initialize();
+    };
+}
+
